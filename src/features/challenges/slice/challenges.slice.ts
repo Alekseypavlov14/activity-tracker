451ecc5dc10417ec90fa44ebdac0a20503d5d751
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-11-09 22:37:50 +0200

    add reducers for challenges feature

--- src/features/challenges/slice/challenges.slice.ts
+++ src/features/challenges/slice/challenges.slice.ts
@@ -1,7 +1,9 @@
-import { AppState } from '@store'
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { ChallengesActions } from './challenges.actions'
 import { ChallengeSubject } from '../types/subject.entity'
 import { LocalStorage } from '@utils/LocalStorage'
+import { generateId } from "@utils/generateId"
+import { AppState } from '@store'
 
 const ChallengesStorage = new LocalStorage<ChallengeSubject>('challenges')
 
@@ -16,7 +18,40 @@ const initialState: InitialState = {
 const challengesSlice = createSlice({
   name: 'challenges',
   initialState,
-  reducers: {}
+  reducers: {
+    create(state, action: PayloadAction<ChallengesActions.Create>) {
+      const challengeSubjectData = action.payload
+
+      const ids = state.list.map(subject => subject.id)
+
+      const newChallengeSubject: ChallengeSubject = {
+        ...challengeSubjectData, 
+        id: generateId(ids)
+      }
+
+      state.list.unshift(newChallengeSubject)
+      ChallengesStorage.setValue(state.list)
+    },
+    update(state, action: PayloadAction<ChallengesActions.Update>) {
+      const newValues = action.payload.newValues
+      const subjectId = action.payload.id
+
+      const subject = state.list.find(subject => subject.id === subjectId)
+
+      if (!subject) return state
+
+      Object.assign(subject, newValues)
+
+      ChallengesStorage.setValue(state.list)
+    },
+    delete(state, action: PayloadAction<ChallengesActions.Delete>) {
+      const subjectId = action.payload.id
+
+      state.list = state.list.filter(subject => subject.id !== subjectId)
+
+      ChallengesStorage.setValue(state.list)
+    } 
+  }
 })
 
 export const challengesReducer = challengesSlice.reducer
